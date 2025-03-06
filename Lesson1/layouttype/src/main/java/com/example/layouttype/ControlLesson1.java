47a4e77e1fb3f5817821685292deacefa854a8a9
MoonFlower18 <juravleva.yuliya202@gmail.com>
2025-03-06 23:03:56 +0300

    test

---
+++ Lesson1/layouttype/src/main/java/com/example/layouttype/ControlLesson1.java
@@ -0,0 +1,13 @@
+package com.example.layouttype;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ControlLesson1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.control_lesson1);
+    }
+}
+
---
+++ Lesson1/layouttype/src/main/java/com/example/layouttype/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.layouttype;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Button butn1 = findViewById(R.id.navbut1);
+        Button butn2 = findViewById(R.id.navbut2);
+        Button butn3 = findViewById(R.id.navbut3);
+        Button butn4 = findViewById(R.id.navbut4);
+
+        butn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LinearActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        butn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, TableActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        butn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ConstraintLayout.class);
+                startActivity(intent);
+            }
+        });
+
+        butn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ControlLesson1.class);
+                startActivity(intent);
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
