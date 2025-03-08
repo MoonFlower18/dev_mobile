0ab8a4ea418be32f90b2e4a046cfd283ab0b3c49
MoonFlower18 <juravleva.yuliya202@gmail.com>
2025-03-08 16:58:49 +0300

    set child headers

--- Lesson1/layouttype/src/main/java/com/example/layouttype/MainActivity.java
+++ Lesson1/layouttype/src/main/java/com/example/layouttype/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.layouttype;
 
 import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
