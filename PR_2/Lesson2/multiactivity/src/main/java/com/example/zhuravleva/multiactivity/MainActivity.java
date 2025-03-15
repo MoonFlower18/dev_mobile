2c134f161d1638edc34cde6b38a05a1b666a7ae2
MoonFlower18 <juravleva.yuliya202@gmail.com>
2025-03-15 23:55:26 +0300

    [ch_2] intent from act_main -> second_act

--- PR_2/Lesson2/multiactivity/src/main/java/com/example/zhuravleva/multiactivity/MainActivity.java
+++ PR_2/Lesson2/multiactivity/src/main/java/com/example/zhuravleva/multiactivity/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.zhuravleva.multiactivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,10 +21,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    public void onClickNewActivity(View view) {
+        Intent intent = new Intent(this, SecondActivity.class);
+
+        EditText editText = findViewById(R.id.edit_text);
+        Button sendButton = findViewById(R.id.send_button);
+        String textToSend = editText.getText().toString();
+
+        intent.putExtra("key", textToSend);
+
+//        intent.putExtra("key", "MIREA - ЖУРАВЛЕВА ЮЛИЯ СЕРГЕЕВНА");
+        startActivity(intent);
+    }
+
 }
--- PR_2/Lesson2/multiactivity/src/main/java/com/example/zhuravleva/multiactivity/SecondActivity.java
+++ PR_2/Lesson2/multiactivity/src/main/java/com/example/zhuravleva/multiactivity/SecondActivity.java
@@ -1,6 +1,8 @@
 package com.example.zhuravleva.multiactivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,12 +11,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.second_activity);
+
+        TextView textView = findViewById(R.id.text_view);
+
+        String text = (String) getIntent().getSerializableExtra("key");
+        textView.setText(text);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
