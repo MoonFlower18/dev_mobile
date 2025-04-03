e18150c45492cdc6af308522f34a16012a3e448c
MoonFlower18 <juravleva.yuliya202@gmail.com>
2025-04-03 17:13:42 +0300

    [ch_2] add module SimpleFragmentApp + fragments - FirstFragment, SecondFragment

--- PR_3/Lesson3/simplefragmentapp/src/main/java/com/mirea/zhuravleva/simplefragmentapp/MainActivity.java
+++ PR_3/Lesson3/simplefragmentapp/src/main/java/com/mirea/zhuravleva/simplefragmentapp/MainActivity.java
@@ -1,12 +1,16 @@
 package com.mirea.zhuravleva.simplefragmentapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +23,24 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Fragment fragment1 = new FirstFragment();
+        Fragment fragment2 = new SecondFragment();
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        Button btnFirstFragment = (Button) findViewById(R.id.btnFirstFragment);
+        btnFirstFragment.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().replace(R.id.fragmentContainerView,
+                        fragment1).commit();
+            }
+        });
+        Button btnSecondFragment = (Button) findViewById(R.id.btnSecondFragment);
+        btnSecondFragment.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                fragmentManager.beginTransaction().replace(R.id.fragmentContainerView,
+                        fragment2).commit();
+            };
         });
     }
 }
