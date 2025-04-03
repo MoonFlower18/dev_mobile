af194cea9ca97a32370dc0a1862f0fd2f231742e
MoonFlower18 <juravleva.yuliya202@gmail.com>
2025-04-03 20:40:14 +0300

    [ch_2] add full fragments + land

--- PR_3/Lesson3/simplefragmentapp/src/main/java/com/mirea/zhuravleva/simplefragmentapp/MainActivity.java
+++ PR_3/Lesson3/simplefragmentapp/src/main/java/com/mirea/zhuravleva/simplefragmentapp/MainActivity.java
@@ -2,45 +2,30 @@ package com.mirea.zhuravleva.simplefragmentapp;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+
+import com.mirea.zhuravleva.simplefragmentapp.FirstFragment;
+import com.mirea.zhuravleva.simplefragmentapp.R;
+import com.mirea.zhuravleva.simplefragmentapp.SecondFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-
-        Fragment fragment1 = new FirstFragment();
-        Fragment fragment2 = new SecondFragment();
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        Button btnFirstFragment = (Button) findViewById(R.id.btnFirstFragment);
-        btnFirstFragment.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                fragmentManager.beginTransaction().replace(R.id.fragmentContainerView,
-                        fragment1).commit();
-            }
-        });
-        Button btnSecondFragment = (Button) findViewById(R.id.btnSecondFragment);
-        btnSecondFragment.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                fragmentManager.beginTransaction().replace(R.id.fragmentContainerView,
-                        fragment2).commit();
-            };
-        });
+    }
+    public void openFirstFragment(View view) {
+        FirstFragment firstFragment = new FirstFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, firstFragment)
+                .commit();
+    }
+    public void openSecondFragment(View view) {
+        SecondFragment secondFragment = new SecondFragment();
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, secondFragment)
+                .commit();
     }
 }
