cc6f232b13996131b74e2f8fb8a00965431013b1
MoonFlower18 <juravleva.yuliya202@gmail.com>
2025-04-13 15:05:24 +0300

    [ch_3] add data info + web fragment

--- PR_3/Lesson3/mireaproject/src/main/java/com/mirea/zhuravleva/mireaproject/WebViewFragment.java
+++ PR_3/Lesson3/mireaproject/src/main/java/com/mirea/zhuravleva/mireaproject/WebViewFragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +59,11 @@ public class WebViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_web_view, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_web_view, container, false);
+        WebView myWebView = (WebView) view.findViewById(R.id.webview);
+        myWebView.loadUrl("https://www.mirea.ru");
+
+        return view;
     }
 }
