5dc9093378bb51427238fc5829a24009daa8b9f7
MoonFlower18 <juravleva.yuliya202@gmail.com>
2025-05-01 17:42:55 +0300

    [ch_3] add base task

--- PR_4/Lesson4/looper/src/main/java/com/mirea/zhuravleva/looper/MainActivity.java
+++ PR_4/Lesson4/looper/src/main/java/com/mirea/zhuravleva/looper/MainActivity.java
@@ -1,6 +1,11 @@
 package com.mirea.zhuravleva.looper;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,15 +13,38 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.mirea.zhuravleva.looper.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
 
+    private ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        
+        binding	= ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
+        Handler mainThreadHandler = new Handler(Looper.getMainLooper())	{
+            @Override
+            public void handleMessage(Message msg) {
+                Log.d(MainActivity.class.getSimpleName(), "Task execute. This is result:	" + msg.getData().getString("result"));
+            }
+        };
+        MyLooper myLooper = new	MyLooper(mainThreadHandler);
+        myLooper.start();
 
+        binding.editTextTextMirea.setText("Мой номер по списку №13");
+        binding.buttonMirea.setOnClickListener(new	View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Message	msg = Message.obtain();
+                Bundle bundle = new	Bundle();
+                bundle.putString("KEY",	"mirea");
+                msg.setData(bundle);
+                myLooper.mHandler.sendMessage(msg);
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
