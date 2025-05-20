a0a3cda8e1bec0a0d9b6c00f72541d60722d6159
MoonFlower18 <juravleva.yuliya202@gmail.com>
2025-05-20 13:54:34 +0300

    [ch_6] ready worker

--- PR_4/Lesson4/workmanager/build.gradle.kts
+++ PR_4/Lesson4/workmanager/build.gradle.kts
@@ -16,6 +16,10 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -40,4 +44,8 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    implementation("androidx.work:work-runtime:2.10.1")
+    implementation ("androidx.concurrent:concurrent-futures:1.2.0")
+
 }
--- PR_4/Lesson4/workmanager/src/main/java/com/mirea/zhuravleva/workmanager/MainActivity.java
+++ PR_4/Lesson4/workmanager/src/main/java/com/mirea/zhuravleva/workmanager/MainActivity.java
@@ -7,6 +7,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.work.Constraints;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkManager;
+import androidx.work.WorkRequest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +20,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+//        Constraints constraints = new Constraints.Builder()
+//                .build();
+
+        Constraints constraints	= new Constraints.Builder()
+                .setRequiredNetworkType(NetworkType.UNMETERED)
+                .setRequiresCharging(true)
+                .build();
+
+        WorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(UploadWorker.class)
+                .setConstraints(constraints).build();
+        WorkManager.getInstance(this).enqueue(uploadWorkRequest);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
