3354f9eb156023fb94e6af9c557feefdb35e407b
MoonFlower18 <juravleva.yuliya202@gmail.com>
2025-05-21 04:26:48 +0300

    [design] all navigation

--- PR_4/Lesson4/app/src/main/java/com/mirea/zhuravleva/Lesson4/MainActivity.java
+++ PR_4/Lesson4/app/src/main/java/com/mirea/zhuravleva/Lesson4/MainActivity.java
@@ -1,9 +1,11 @@
 package com.mirea.zhuravleva.Lesson4;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +23,60 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding	= ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.navbut2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName("com.mirea.zhuravleva.thread", "com.mirea.zhuravleva.thread.MainActivity"));
+                startActivity(intent);
+            }
+        });
+
+        binding.navbut3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName("com.mirea.zhuravleva.data_thread", "com.mirea.zhuravleva.data_thread.MainActivity"));
+                startActivity(intent);
+            }
+        });
+
+        binding.navbut4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName("com.mirea.zhuravleva.looper", "com.mirea.zhuravleva.looper.MainActivity"));
+                startActivity(intent);
+            }
+        });
+
+        binding.navbut5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName("com.mirea.zhuravleva.cryptoloader", "com.mirea.zhuravleva.cryptoloader.MainActivity"));
+                startActivity(intent);
+            }
+        });
+
+        binding.navbut6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName("com.mirea.zhuravleva.serviceapp", "com.mirea.zhuravleva.serviceapp.MainActivity"));
+                startActivity(intent);
+            }
+        });
+
+        binding.navbut7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setComponent(new ComponentName("com.mirea.zhuravleva.workmanager", "com.mirea.zhuravleva.workmanager.MainActivity"));
+                startActivity(intent);
+            }
+        });
 
         EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
